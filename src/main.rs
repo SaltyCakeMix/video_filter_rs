fdb9b6fc6103dbc42ad7745f2cb501798a8b2020
SaltyCakeMix <justinle33333@gmail.com>
2025-04-14 18:31:41 -0700

    seomething

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,56 @@
-use ffmpeg_next::{codec, decoder, encoder, format::{self, Pixel}, frame, picture, software::scaling::{Context, Flags}, Dictionary, Packet, Rational};
+use ffmpeg_the_third::{codec::{self, Parameters}, decoder, encoder, format::{self, Pixel}, frame, picture, software::scaling::{Context, Flags}, Dictionary, Packet, Rational};
 use rusttype::{point, Font, Scale};
 
-fn decode_frames(decoder: &mut decoder::Video, encoder: &mut encoder::Video, scaler: &mut Context) {
+struct RenderData {
+    w: usize,
+    h: usize,
+    x: Vec<usize>,
+    y: Vec<usize>,
+}
+
+impl RenderData {
+    fn new(r_w: u32, r_h: u32, f_w: u32, f_h: u32) -> Self {
+        Self {
+            w: r_w as usize, h: r_h as usize,
+            x: (0..r_w).map(|x| (x * f_w) as usize).collect(),
+            y: (0..r_h).map(|x| (x * f_h) as usize).collect(),
+        }
+    }
+}
+
+struct NominalData {
+    w: usize,
+    h: usize,
+    font_w: usize,
+    font_h: usize,
+}
+
+fn decode_frames(decoder: &mut decoder::Video, encoder: &mut encoder::Video, scaler: &mut Context, char_set: &[Vec<Vec<u8>>], render_data: &RenderData, nom_data: &NominalData) {
     let mut frame = frame::Video::empty();
+    let lum_to_char = char_set.len() as f32 / 256.;
     while decoder.receive_frame(&mut frame).is_ok() {
         let mut scaled_frame = frame::Video::empty();
         scaler.run(&frame, &mut scaled_frame).unwrap();
-        // let mut y = scaled_frame.data_mut(0);
+        let luminosity = scaled_frame.data_mut(0);
 
         let mut new_frame = frame::Video::new(Pixel::YUV420P, 1920, 1080);
+        let bytes = new_frame.data_mut(0);
+        let mut i = 0;
+        for y in render_data.y.iter() {
+            for x in render_data.x.iter() {
+                let char_index = luminosity[i] as f32 * lum_to_char;
+                let stamp = &char_set[char_index as usize];
 
-        let timestamp = frame.timestamp();
-        new_frame.set_pts(timestamp);
+                for (char_y, line) in stamp.iter().enumerate() {
+                    let start = x + (y + char_y)*nom_data.w;
+                    let end = start + nom_data.font_w;
+                    bytes[start..end].copy_from_slice(line);
+                }
+                i += 1;
+            }
+        }
+
+        new_frame.set_pts(frame.timestamp());
         new_frame.set_kind(picture::Type::None);
         encoder.send_frame(&new_frame).unwrap();
     }
@@ -72,7 +111,7 @@ fn main() {
     let src = "src.mp4";
     let dst = "dst.mp4";
     let dst_h = 1080;
-    let render_h = 60;
+    let render_h = 50;
     let font_path = include_bytes!("../MonospaceTypewriter.ttf");
     let char_set = " .-^:~/*+=?%##&$$@@@@@@@@@@@@";
 
@@ -80,7 +119,7 @@ fn main() {
     let mut in_ctx = format::input(&src).unwrap();
 
     // Finds video stream
-    let in_stream = in_ctx.streams().best(ffmpeg_next::media::Type::Video)
+    let in_stream = in_ctx.streams().best(ffmpeg_the_third::media::Type::Video)
         .expect("Could not find a proper video stream");
     let in_stream_index = in_stream.index();
     let in_time_base = in_stream.time_base();
@@ -101,8 +140,7 @@ fn main() {
     
     // Font
     let (font_w, char_set) = construct_char_set(font_path, char_set, font_h);
-    let font_aspect_ratio = font_w as f32 / font_h as f32;
-    let render_w = ((render_h * src_w / src_h) as f32 / font_aspect_ratio) as u32;
+    let render_w = dst_w / font_w;
 
     // Output
     let mut out_ctx = format::output(&dst).unwrap();
@@ -114,51 +152,60 @@ fn main() {
     let out_stream_index = out_stream.index();
 
     // Creates encoder
-    let mut encoder = codec::context::Context::new_with_codec(codec)
-            .encoder().video().unwrap();
+    let mut encoder = codec::context::Context::from_parameters(out_stream.parameters())
+            .unwrap().encoder().video().unwrap();
     encoder.set_width(dst_w);
     encoder.set_height(dst_h);
     encoder.set_aspect_ratio(decoder.aspect_ratio());
-    encoder.set_format(src_fmt);
+    encoder.set_format(Pixel::YUV420P);
     encoder.set_frame_rate(Some(in_stream.avg_frame_rate()));
     encoder.set_time_base(in_time_base);
 
     let mut x264_opts = Dictionary::new();
-    x264_opts.set("preset", "medium");
+    x264_opts.set("preset", "veryslow");
+    x264_opts.set("crf", "18");
 
-    let mut opened_encoder = encoder
-        .open_with(x264_opts)
+    let mut encoder = encoder
+        .open_as_with(codec, x264_opts)
         .expect("error opening x264 with supplied settings");
-    out_stream.set_parameters(&opened_encoder);
+    out_stream.set_parameters(Parameters::from(&encoder));
     
-    let dst_fmt = opened_encoder.format();
+    let dst_fmt = encoder.format();
     let out_time_base = out_stream.time_base();
 
 
     // Parses video
-    // let mut scaler = Context::get(
-    //     src_fmt,
-    //     src_w, src_h,
-    //     dst_fmt,
-    //     render_w, render_h,
-    //     Flags::FAST_BILINEAR,
-    // ).unwrap();
+    let mut scaler = Context::get(
+        src_fmt,
+        src_w, src_h,
+        dst_fmt,
+        render_w, render_h,
+        Flags::FAST_BILINEAR,
+    ).unwrap();
 
-    // out_ctx.set_metadata(in_ctx.metadata().to_owned());
-    // out_ctx.write_header().unwrap();
-    // for (stream, packet) in in_ctx.packets() {
-    //     if stream.index() == in_stream_index {
-    //         decoder.send_packet(&packet).unwrap();
-    //     }
-    //     decode_frames(&mut decoder, &mut opened_encoder, &mut scaler);
-    //     encode_frames(&mut opened_encoder, out_stream_index, in_time_base, out_time_base, &mut out_ctx);
-    // }
-    // decoder.send_eof().unwrap();
-    // decode_frames(&mut decoder, &mut opened_encoder, &mut scaler);
-    // opened_encoder.send_eof().unwrap();
-    // encode_frames(&mut opened_encoder, out_stream_index, in_time_base, out_time_base, &mut out_ctx);
-  
-    // out_ctx.write_trailer().unwrap();
+    out_ctx.set_metadata(in_ctx.metadata().to_owned());
+    out_ctx.write_header().unwrap();
+
+    
+    let render_data = RenderData::new(render_w, render_h, font_w, font_h);
+    println!("{}, {}", render_data.x.len(), render_data.y.len());
+    println!("{:?}", render_data.x);
+    println!("{:?}", render_data.y);
+    let nom_data = NominalData {w: dst_w as usize, h: dst_h as usize, font_w: font_w as usize, font_h: font_h as usize};
+    for (stream, packet) in in_ctx.packets().filter_map(Result::ok) {
+        if stream.index() == in_stream_index {
+            decoder.send_packet(&packet).unwrap();
+        }
+        decode_frames(&mut decoder, &mut encoder, &mut scaler, &char_set, &render_data, &nom_data);
+        encode_frames(&mut encoder, out_stream_index, in_time_base, out_time_base, &mut out_ctx);
+    }
+
+    // Close file
+    decoder.send_eof().unwrap();
+    decode_frames(&mut decoder, &mut encoder, &mut scaler, &char_set, &render_data, &nom_data);
+    encoder.send_eof().unwrap();
+    encode_frames(&mut encoder, out_stream_index, in_time_base, out_time_base, &mut out_ctx);
+    out_ctx.write_trailer().unwrap();
 
     // let audio_stream = input_context.streams().best(ffmpeg_next::media::Type::Audio).unwrap();
     // let audio_stream_index = audio_stream.index();
